635e24d59ba6602b4b995b147c53287998722fc6
Alison Luiz <alison_luiz@outlook.com.br>
2023-02-20 19:42:55 -0300

    chore: exercises 01 to 10 from the list

---
+++ Lista de Exercicios/Lista 01/exercicio_01.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main() {
+
+    printf("Testando um algoritmo\n");
+    
+    printf("Testando um algoritmo\nLinguagem C\n");
+    
+    system("pause");
+}
