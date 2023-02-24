6d1c3b34b0884c4114f69a3a00a310b213286750
Alison Luiz <alison_luiz@outlook.com.br>
2023-02-23 21:44:23 -0300

    feat: repeating structure

---
+++ Aulas/Aula 04 - Laços de Repetição/do while/index.cpp
@@ -0,0 +1,24 @@
+// do while - faça enquanto
+
+// do
+// {
+//     commands;
+// } while (condition);
+
+#include <stdlib.h>
+#include <stdio.h>
+
+main() {
+
+int x = 1;
+
+do {
+    printf("%d \n", x);
+    x++;
+} while (x<=10);
+
+printf("%d \n", x);
+
+system("pause");
+
+}
---
+++ Aulas/Aula 04 - Laços de Repetição/while/index.cpp
@@ -0,0 +1,21 @@
+// While - enquanto
+
+// while (condition) {
+//     commands;
+// }
+
+#include <stdlib.h>
+#include <stdio.h>
+
+main() {
+
+int x = 1;
+
+while (x<=10) {
+    printf("%d \n",x);
+    x++;
+}
+
+system("pause");
+
+}
